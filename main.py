f0344451cf24aec509aabec3960dc39bf9333875
P2101 <98814765+P2101@users.noreply.github.com>
2023-02-08 18:43:59 +0100

    Create main.py

---
+++ main.py
@@ -0,0 +1,30 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+from typing import Optional
+
+# INSTANCIAMOS LA CLASE
+app = FastAPI()
+# 'http://127.0.0.1:8000/'
+
+# BASEMODEL VALIDACION DE DATOS
+class Libro(BaseModel):
+    title: str
+    authon: str
+    pages: int
+    year: Optional[int]
+
+
+
+# El @ es un decorador para registrar la función e INDICAMOS LA RUTA, EN ESTE CASO ES LA RAIZ
+@app.get('/')
+def index():
+    return {"Funcion": "principal"}
+
+@app.get('/libros/{id}')
+def mostrar_libros(id: int):
+    return {'data': id}
+
+
+@app.post('/libros')
+def insertar_libro(libro: Libro):
+    return {'message': f'el libro {libro.title} ha sido insertado'}
