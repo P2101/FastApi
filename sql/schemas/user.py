88e2f0bfe51fb7d31dbabb3d7bd31c5e349d2184
Pau Pons <98814765+P2101@users.noreply.github.com>
2024-02-14 11:55:07 +0100

    Update user.py

--- sql/schemas/user.py
+++ sql/schemas/user.py
@@ -1,7 +1,8 @@
 from pydantic import BaseModel
 from typing import Optional
 
-class User(BaseModel):
+# Para Validar los datos de entrada y salida en las solicitudes HTTP
+class User(BaseModel): 
     id: Optional[str]
     name: str
     email: str
